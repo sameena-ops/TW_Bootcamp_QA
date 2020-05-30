f8059738a1cb4f619a1b3adacdcc11b0142f681f
sameenasyed <sameena9010@gmail.com>
2020-05-30 21:09:45 +0530

    added new project

--- Bootcamp_UI/src/test/java/com/java/juiceshop/UI_JuiceShopTest.java
+++ Bootcamp_UI/src/test/java/com/java/juiceshop/UI_JuiceShopTest.java
@@ -4,21 +4,35 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class UI_JuiceShopTest {
 
     String email = "b@gmail.com";
     String password = "abc@gmail.com";
-
+    WebDriver driver;
     @Test
     public void registerUser(){
         WebDriverManager.chromedriver().setup();
-     WebDriver driver = new ChromeDriver();
+
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+
+        try {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+                    capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
      //driver.get("http://localhost:3000/#/");
         driver.manage().window().maximize();
-        driver.get("http://localhost:3000/#/register");
+        driver.get("http://juice-shop:3000/#/register");
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.findElement(By.xpath("//*[@id=\"mat-dialog-0\"]/app-welcome-banner/div/div[2]/button[2]")).click();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
