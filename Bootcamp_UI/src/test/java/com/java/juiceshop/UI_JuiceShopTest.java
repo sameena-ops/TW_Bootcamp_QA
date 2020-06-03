bdcc9b20ebaba558ebd9ba6a8d32db7225bd594a
sameenasyed <sameena9010@gmail.com>
2020-06-03 21:33:00 +0530

    changed annotation

--- Bootcamp_UI/src/test/java/com/java/juiceshop/UI_JuiceShopTest.java
+++ Bootcamp_UI/src/test/java/com/java/juiceshop/UI_JuiceShopTest.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -23,10 +24,11 @@ public class UI_JuiceShopTest {
     String password = "abc@gmail.com";
     //  WebDriver driver;
 
-    @BeforeClass
+    @BeforeEach
     public void setup(){
         BrowserFactory.getInstance("remoteChrome");
     }
+
     @Test
     public void testFirstTimeRegister() {
         BrowserFactory.driver.get("http://localhost:3000/#/register");
